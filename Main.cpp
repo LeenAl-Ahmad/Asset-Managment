ca524b31c8dad46c86aca101d316077080597ccd
Leen Al-Ahmad <alahmadl@sheridancollege.ca>
2025-01-29 02:10:38 -0500

    Descriptivee Debug, adding typeid(T).name()

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@ int main()
     ofstream writeStream("level.bin", ios::out | ios::binary);
     level->Serialize(writeStream);
     writeStream.close();
-    cout << "Level To Save:";
+    cout << "Level To Save: ";
     level->ToString();
     delete level;
     cout << endl;
@@ -17,7 +17,7 @@ int main()
     ifstream readStream("level.bin", ios::in | ios::binary);
     loadedLevel->Deserialize(readStream);
     readStream.close();
-    cout << "Loaded Level:";
+    cout << "Loaded Level: ";
     loadedLevel->ToString();
 
     delete loadedLevel;

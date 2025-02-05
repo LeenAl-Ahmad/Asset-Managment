6fd8b8c59eb6e4d61452b70c6bd88c76a902602f
Leen Al-Ahmad <alahmadl@sheridancollege.ca>
2025-02-05 09:14:23 -0500

    FileChunk header added

---
+++ FileChunk.h
@@ -0,0 +1,27 @@
+#ifndef FILECHUNK_H
+#define FILECHUNK_H
+
+#include "Resource.h"
+
+class FileChunk : public Resource
+{
+public:
+	FileChunk();
+	virtual ~FileChunk();
+
+	Asset* GetChunk() { return m_chunk; }
+	void SetChunk(Asset* _p) { m_chunk = _p; }
+
+	//Methods 
+	virtual void Serialize(std::ostream& _s);
+	virtual void Deeserialize(std::istream& _s);
+	virtual void ToString();
+	virtual void AssignNonDefaultValues();
+
+	static ObjectPool<FileChunk>* Pool;
+
+	private:
+		Asset* m_chunk;
+};
+
+#endif //FILECHUNK_H
